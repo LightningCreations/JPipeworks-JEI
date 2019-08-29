a9aaaf49461e2ffb8c5ef4473ebb2c69d88a96db
Connor Horman <chorman64@gmail.com>
2019-08-29 16:45:06 -0400

    Add JEIServiceProvider for Pipeworks, using "pipeworks" as the name.

---
+++ src/main/java/com/lightning/jpipeworks/lcjei/PipeworksJEIServiceProvider.java
@@ -0,0 +1,47 @@
+package com.lightning.jpipeworks.lcjei;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import com.lightning.jpipeworks.Engine;
+import com.lightning.jpipeworks.Game;
+
+import github.lightningcreations.lcjei.IEngineInterface;
+import github.lightningcreations.lcjei.service.JEIServiceProvider;
+
+public class PipeworksJEIServiceProvider implements JEIServiceProvider<Game> {
+
+	public PipeworksJEIServiceProvider() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Class<Game> getGameClass() {
+		// TODO Auto-generated method stub
+		return Game.class;
+	}
+
+	@Override
+	public String getEngineName() {
+		// TODO Auto-generated method stub
+		return "pipeworks";
+	}
+
+	@Override
+	public IEngineInterface<Game> newEngine(Game game) {
+		// TODO Auto-generated method stub
+		return new PipeworksEngineInterface(game);
+	}
+
+	@Override
+	public Optional<IEngineInterface<Game>> getActiveEngineInterface() {
+		// TODO Auto-generated method stub
+		return getActiveEngines().findAny();
+	}
+
+	@Override
+	public Stream<IEngineInterface<Game>> getActiveEngines() {
+		return Engine.getRunningPipeworksEngines().map(PipeworksEngineInterface::new);
+	}
+
+}
